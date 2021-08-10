ac7e76741eadef2ab3e5f9230bcc13028faace22
Julie Ann Meng <JulieAnnMeng@gmail.com>
2021-08-10 11:14:54 -0400

    commit tree c9e8f578ddbad62c055b6a97f80d3632d0a9add6

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = array => array.slice(0, 2);
+
+const returnLastTwoDrivers = array => array.slice(array.length-2, array.length);
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(multiplier){
+    return function(fare){
+        return fare * multiplier;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, func){
+    return func(drivers);
+}
